4a4fc63fed67ec397cdd9bb31913019537155189
AboveVale <brodypace@gmail.com>
2026-01-11 14:53:15 -0700

    Delete Start.bat

--- Start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "C:\Users\brody\Documents\1C. Coding\Websites\CardsOfRapport"
-echo Starting server...
-start powershell -NoExit -Command "node server.js"
-timeout /t 2 >nul
-echo Opening browser...
-start http://localhost:3000/
-exit
